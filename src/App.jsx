9c84355ca49868ff808aac8926af52535d0b3d7a
User-Takhirov <tohirovfayruz@gmail.com>
2025-05-27 15:27:45 +0500

    updated just now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { Container } from "./Components/Container";
+import { Brands } from "./container/Brands";
+import { Faq } from "./container/Faq";
+import { Footer } from "./container/Footer";
+import { Hero } from "./container/Hero";
+import { Navbar } from "./container/navbar";
+import { Opportunity } from "./container/Opportunity";
+import { Plan } from "./container/Plan";
+import { Questions } from "./container/Questions";
+
 function App() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <Container>
+        <Navbar />
+        <Hero />
+        <Brands />
+        <Opportunity />
+        <Questions />
+        <Plan />
+        <Faq />
+        <Footer />
+      </Container>
+    </>
+  );
 }
 
 export default App;
